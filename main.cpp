7c1b0b7be78cb0e532f42129170f2f57cd69abc2
Sergey Zaporozhenko <sergey.deadmilk@yandex.ru>
2022-11-27 17:23:40 +0700

    added file1.txt, file2.txt

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@ int main()
 	std::cout<<"Hello world!" <<std::endl;
 	std::cout<<"Second line"<<std::endl;
 	std::cout <<"Thir line"<<std::endl;
+	std::cout <<"Thir line"<<std::endl;
 	return 0;
 }
 
